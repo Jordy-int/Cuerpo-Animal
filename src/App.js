8053cf544b901019b11f17663aad0d8602ccb68f
Jordy-int <jordy.sneider13@gmail.com>
2025-10-23 23:04:36 -0500

    Basic CSS MainPage

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
 
+import bovino from './Assets/Bovino.jpg'
+import canino from './Assets/Canino.jpg'
+import Style from './CSS/Main.module.css'
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+
+      <header>
+        <h1 className='text-center' >Enciclopedia Animal</h1>
       </header>
-    </div>
+      <h2 className='text-center' >Anatomía Disponible</h2>
+
+      <main className='d-flex justify-content-evenly' >
+
+        <figure className='figure text-center rounded p-6'>
+          <img className='figure-img img-fluid rounded p-6' src={bovino} alt='Imagen de animal' />
+          <figcaption className='figure-caption fs-3' >Bovino</figcaption>
+        </figure>
+
+        <figure className='figure text-center rounded p-6 '>
+          <img className='figure-img img-fluid rounded p-6 ' src={canino} alt='Imagen de animal' />
+          <figcaption className='figure-caption fs-3' >Canino</figcaption>
+        </figure>
+
+
+
+      </main>
+
+      <footer>
+        <h4>Derechos reservados a ...</h4>
+      </footer>
+
+    </>
+
   );
 }
 
